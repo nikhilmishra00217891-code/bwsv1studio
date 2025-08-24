aa8bc19b69a6508d127c19a23570edf2d7e0efa4
Pratyush Priyank <bwstechmain@gmail.com>
2025-08-24 12:39:06 +0000

    Ok u have to increase the length of the page itself to pull this off !

--- src/app/admin/users/UserTableClient.tsx
+++ src/app/admin/users/UserTableClient.tsx
@@ -266,7 +266,7 @@ export function UserTableClient({ initialUsers }: { initialUsers: UserProfile[] 
         </div>
 
       <div className="border rounded-lg">
-        <ScrollArea className="h-[calc(100vh-22rem)]">
+        <ScrollArea>
           <Table>
             <TableHeader className="sticky top-0 bg-background z-10">
               <TableRow>
