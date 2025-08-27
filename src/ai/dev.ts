131ea010c323148030ab186f31dba834ef8faa89
Pratyush Priyank <bwstechmain@gmail.com>
2025-08-27 14:53:03 +0000

    Why the heck did u create .env.local file we already had .env file with

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/answer-questions-about-courses.ts';
 import '@/ai/flows/help-students-find-relevant-courses.ts';
 import '@/ai/flows/summarize-course-content.ts';
