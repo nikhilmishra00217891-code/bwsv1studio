6c38020ae2b042c8ee801288ce90f3ff60275478
Pratyush Priyank <bwstechmain@gmail.com>
2025-08-30 15:18:51 +0000

    Still not dude , I am currently in recoded section!

--- src/components/courses/CourseContent.tsx
+++ src/components/courses/CourseContent.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import type { Course, Lesson, Subject, Chapter } from "@/types";
@@ -134,7 +135,7 @@ const LectureView = ({ lesson }: { lesson: Lesson }) => {
                 {lesson.type === 'video' && videoId ? (
                      <iframe
                         className="w-full h-full"
-                        src={`https://www.youtube.com/embed/${videoId}?rel=0&showinfo=0&autoplay=1`}
+                        src={`https://www.youtube.com/embed/${videoId}?rel=0&autoplay=1`}
                         title="YouTube video player"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowFullScreen>
