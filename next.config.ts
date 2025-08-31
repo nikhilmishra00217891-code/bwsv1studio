c71fce20cca87bc7287abcf8489bbf351c24b419
Pratyush Priyank <bwstechmain@gmail.com>
2025-08-31 19:48:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'postimg.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
       }
     ],
   },
