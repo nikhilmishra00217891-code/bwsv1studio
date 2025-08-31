6f9b0feeebd36a9ed1b94aecd6d0e6459197be4f
Pratyush Priyank <bwstechmain@gmail.com>
2025-08-31 19:52:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'm.media-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
       }
     ],
   },
