cb394906e9198d114f665dde5fc4f0c54afae3a3
Pratyush Priyank <bwstechmain@gmail.com>
2025-09-08 18:30:34 +0000

    Now next step is Toolkit which we already have , just make it visible to

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
+
+"use client";
+
 import Hero from "@/components/home/NewHero";
 import FeaturedCourses from "@/components/home/FeaturedCourses";
 import Testimonials from "@/components/home/Testimonials";
 
 import AnnouncementSlider from "@/components/home/AnnouncementSlider";
 import QuickActions from "@/components/home/QuickActions";
+import { useAuth } from "@/components/auth/AuthProvider";
 
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
     <div className="flex flex-col space-y-16 md:space-y-24 py-16 md:py-24">
       <AnnouncementSlider />
       <Hero />
       <FeaturedCourses />
-      <QuickActions />
+      {user && <QuickActions />}
       <Testimonials />
     </div>
   );
