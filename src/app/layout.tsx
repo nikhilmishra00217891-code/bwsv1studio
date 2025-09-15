b79eeec62b60b10eb4ed30d7cf9fba5b47626326
Pratyush Priyank <bwstechmain@gmail.com>
2025-09-15 09:53:29 +0000

    Still not buddy , is it that hard huh ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   title: "BiharWaleSirji - Parivaar. Pratishtha. Parivartan.",
   description:
     "India's first platform that teaches like an elder brother, not a stranger.",
-  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
