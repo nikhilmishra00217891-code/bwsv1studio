4e1756d2e5484453a0f38a6a23b1cb1a3b66fb2d
Pratyush Priyank <bwstechmain@gmail.com>
2025-09-15 09:47:25 +0000

    Still not , do one thing simply remove the current favicon ! Don't do an

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
        <head>
-        <link rel="icon" href="https://i.postimg.cc/FR3TT8KL/IMG-20250915-WA0003-1.jpg" />
+        
       </head>
       <body className={`${poppins.variable} font-body antialiased select-none`}>
         <AuthProvider>
