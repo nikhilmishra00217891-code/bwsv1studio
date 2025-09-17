54ec5c34bab1eb4a11a69bdc131ea7f058998c58
Pratyush Priyank <bwstechmain@gmail.com>
2025-09-17 06:17:02 +0000

    Dude why even though the next public api key is there : AIzaSyCnEk3PD_AC

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY})],
   model: 'googleai/gemini-1.5-flash-latest',
 });
