673a21f5d4e2db486d570c69454ce160a15da012
Pratyush Priyank <bwstechmain@gmail.com>
2025-10-12 16:43:53 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Expected first arg

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 
 
 import type { Course, Testimonial, EnrolledCourse, UserProfile, Subject, Chapter, Lesson, LiveChatMessage } from "@/types";
-import { db } from "./firebase";
+import { db } from "./firebase/client";
 import { collection, getDocs, query, where, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc, orderBy, onSnapshot, Timestamp, increment, arrayUnion, limit, arrayRemove } from "firebase/firestore";
 import type { User } from "firebase/auth";
 export * from './data/coupons';
@@ -455,3 +455,5 @@ export const toggleLessonCompletion = async (userId: string, courseId: string, l
         [progressField]: updatedCompletedLessons
     });
 };
+
+    
