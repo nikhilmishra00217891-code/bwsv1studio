af7f7cf8229961eab5279f9200d25d971c3b6fae
Pratyush Priyank <bwstechmain@gmail.com>
2025-10-12 18:25:11 +0000

    Again

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY})],
+  plugins: [googleAI()],
 });
